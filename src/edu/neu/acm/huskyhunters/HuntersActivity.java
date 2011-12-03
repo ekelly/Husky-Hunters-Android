e54c80de9ce91c5f8f1a4c290a4e296513c463a3
Eric Kelly <me@useric.com>
2011-12-02 21:00:39 -0500

    Initial commit

---
+++ src/edu/neu/acm/huskyhunters/HuntersActivity.java
@@ -0,0 +1,13 @@
+package edu.neu.acm.huskyhunters;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HuntersActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
